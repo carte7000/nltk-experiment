c9b04456292b47f39130210dde830dec0f2ac297
carte7000 <simrob7000@gmail.com>
2015-10-23 17:26:19 +0000

    added pair

--- Coocurence.py
+++ Coocurence.py
@@ -13,6 +13,16 @@ def pos(array):
 def stem(sentence):
     st = PorterStemmer()
     return st.stem(sentence)
+    
+def take_all_pair(sentence, space):
+    tokens = tokenize(sentence)
+    pair = []
+    for i in range(0,len(tokens)-(space-1)):
+        if(i+space < len(tokens)):
+            pair.append([tokens[i],tokens[i+space]])
+            
+    return pair
+        
 
 class TestTokenize(unittest.TestCase):
   def test_tokens(self):
@@ -31,6 +41,17 @@ class TestStem(unittest.TestCase):
         sentence = """At eight o'clock on Thursday morning Arthur didn't feel very good."""
         stem_result = stem(sentence)
         self.assertTrue(True)
+        
+class TestPair(unittest.TestCase):
+    def test_do_pair_work(self):
+        sentence = """Hello this is a"""
+        pair_result = take_all_pair(sentence, 1)
+        self.assertEqual([["Hello","this"],["this","is"],["is","a"]], pair_result)
+        
+    def test_do_pair_with_2(self):
+        sentence = """Hello this is a"""
+        pair_result = take_all_pair(sentence, 2)
+        self.assertEqual([["Hello","is"],["this","a"]], pair_result)
 
 print("Running Tests")
 unittest.main()
