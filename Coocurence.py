93cb1d8b69aa42d9da35eb6f75b2b802f65ec2c6
carte7000 <simrob7000@gmail.com>
2015-10-23 19:24:36 +0000

    added frequencies count

--- Coocurence.py
+++ Coocurence.py
@@ -22,7 +22,55 @@ def take_all_pair(sentence, space):
             pair.append([tokens[i],tokens[i+space]])
             
     return pair
+    
+def take_all_pair_array(tagged_array, space):
+    pair = []
+    for i in range(0,len(tagged_array)-(space-1)):
+        if(i+space < len(tagged_array)):
+            pair.append([tagged_array[i],tagged_array[i+space]])
+            
+    return pair
         
+def count_word(word_array):
+    word_dict = dict()
+    for word in word_array:
+        if word not in word_dict:
+            word_dict[word] = 0
+            
+        word_dict[word]+=1
+    
+    return word_dict
+    
+        
+def count_pair(pair_array):
+    pair_dict = dict()
+    for pair in pair_array:
+        if tuple(pair) not in pair_dict:
+            pair_dict[tuple(pair)] = 0
+            
+        pair_dict[tuple(pair)]+=1
+    
+    return pair_dict
+    
+def filter_tag(array, allowedPairTag):
+    pos_result = pos(array)
+    pair_array = take_all_pair_array(pos_result, 1)
+    pair_dict = count_pair(pair_array)
+    
+    result = dict()
+    for key, value in pair_dict.iteritems():
+        if extractPos(list(key)) in allowedPairTag:
+            rekey = [list(list(key)[0])[0],list(list(key)[1])[0]]
+            result[tuple(rekey)] = value
+    
+    return result
+
+def extractPos(tagged_array):
+    result = []
+    for tag in tagged_array:
+        result.append(tag[1])
+        
+    return result
 
 class TestTokenize(unittest.TestCase):
   def test_tokens(self):
@@ -52,6 +100,20 @@ class TestPair(unittest.TestCase):
         sentence = """Hello this is a"""
         pair_result = take_all_pair(sentence, 2)
         self.assertEqual([["Hello","is"],["this","a"]], pair_result)
+        
+class TestCount(unittest.TestCase):
+    def test_do_count_pair_work(self):
+        count_result = count_pair([["a","a"],["a","a"],["b","a"]])
+        self.assertTrue(count_result[tuple(["a","a"])]==2)
+        self.assertTrue(count_result[tuple(["b","a"])]==1)
+        
+class TestFilter(unittest.TestCase):
+    def test_do_filter_pair_work(self):
+        filter_result = filter_tag(tokenize("they refuse to"), [["PRP","VBP"]])
+        #filter_result = filter_tag(count_pair([["they","refuse"],["refuse","to"]]), [["PRP","VBP"]])
+        print(filter_result)
+        self.assertTrue(tuple(["they","refuse"]) in filter_result)
+        self.assertFalse(tuple(["refuse","to"]) in filter_result)
 
 print("Running Tests")
 unittest.main()
