fc3a0e5651cbf3de820979be257b1851056451c5
carte7000 <simrob7000@gmail.com>
2015-10-23 16:59:49 +0000

    added pos

--- Coocurence.py
+++ Coocurence.py
@@ -1,8 +1,13 @@
+#required download punkt, averaged_perceptron_tagger
+
 import nltk
 import unittest
 
 def tokenize(sentence):
 	return nltk.word_tokenize(sentence)
+	
+def pos(array):
+    return nltk.pos_tag(array)
 
 class TestTokenize(unittest.TestCase):
   def test_tokens(self):
@@ -10,6 +15,11 @@ class TestTokenize(unittest.TestCase):
   	tokens = tokenize(sentence)
   	self.assertEqual(tokens, ['At', 'eight', "o'clock", 'on', 'Thursday', 'morning', 'Arthur', 'did', "n't", 'feel', 'very', 'good', '.'])
 
+class TestPos(unittest.TestCase):
+    def test_do_pos_run(self):
+        sentence = """At eight o'clock on Thursday morning Arthur didn't feel very good."""
+        pos_tokens = pos(tokenize(sentence))
+        self.assertTrue(True)
 
 print("Running Tests")
 
